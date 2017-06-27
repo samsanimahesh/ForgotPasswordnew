27d9bcf2e66c19d7b64d9dc9ab5411e350ed9e93
MAHESH <mahesh@MAHESHs-MacBook-Pro.local>
2017-06-28 02:39:07 +0530

    First commit on Forgot password project: added html/servlet files

---
+++ src/com/mahesh/beans/UserDetails.java
@@ -0,0 +1,70 @@
+package com.mahesh.beans;
+
+public class UserDetails {
+	private String firstName;
+	private String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	private String dob;
+	private String emailId;
+	private String phoneNumber;
+	private String password;
+	private String securityQuestion;
+	private String answer;
+	private String address;
+	
+	
+
+}
---
+++ src/com/mahesh/servlets/CustomerRegServlet.java
@@ -0,0 +1,61 @@
+package com.mahesh.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mahesh.beans.UserDetails;
+import com.mahesh.constant.Constants;
+
+/**
+ * Servlet implementation class CustomerRegServlet
+ */
+@WebServlet({ "/CustomerRegServlet"})
+public class CustomerRegServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CustomerRegServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		UserDetails userDetails = createBean(request);
+		
+		
+	}
+
+	private UserDetails createBean(HttpServletRequest request) {
+		UserDetails userDetails = new UserDetails();
+		userDetails.setFirstName(request.getParameter(Constants.FIRST_NAME));
+		userDetails.setLastName(request.getParameter(Constants.LAST_NAME));
+		userDetails.setDob(request.getParameter(Constants.DATE_OF_BIRTH));
+		userDetails.setEmailId(request.getParameter(Constants.EMAIL_ID));
+		userDetails.setPassword(request.getParameter(Constants.PASSWORD));
+		userDetails.setSecurityQuestion(request.getParameter(Constants.SECURITY_QUESTION));
+		userDetails.setAnswer(request.getParameter(Constants.ANSWER));
+		userDetails.setAddress(request.getParameter(Constants.ADDRESS));
+		userDetails.setPhoneNumber(request.getParameter(Constants.PHONE_NUMBER));
+		return userDetails;
+	}
+
+}
